8d9d8a91f7e1320ae8cd41c4fe98113e5517ab47
Stephen C. Pope <stephen@descarteslabs.com>
2020-01-06 15:37:18 -0700

    use mock to keep py3.5 happy

--- setup.py
+++ setup.py
@@ -93,6 +93,7 @@ def do_setup():
             ],
             "tests": [
                 "hypothesis[numpy]==4.24.5",
+                "mock",
                 "pytest==4.3.1",
                 "responses",
             ]
